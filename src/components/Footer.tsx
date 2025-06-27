35ba8e1f7b6c981eb5ac5b0309e9263b30dcfee1
zeruku <zrcblue@gmail.com>
2025-06-27 11:18:52 +0900

    [add] App.tsxにFooterコンポーネントを追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function footer() {
+    return (
+        <footer className="p-10 bg-gray-100">
+            <div className="flex items-center justify-center gap-10 mb-4">
+                <a
+                    href="/terms"
+                    className="text-sm text-gray-600 hover:text-gray-800"
+                >
+                    規約
+                </a>
+                <a
+                    href="/privacy"
+                    className="text-sm text-gray-600 hover:text-gray-800"
+                >
+                    プライバシーポリシー
+                </a>
+                <a
+                    href="/about"
+                    className="text-sm text-gray-600 hover:text-gray-800"
+                >
+                    会社概要
+                </a>
+            </div>
+            <p className="text-sm text-gray-600 text-center">
+                &copy; 2025 RAMEEEN
+            </p>
+        </footer>
+    );
+}
