fbd61641a0d6190605e32ff4f7d1c52452ed08dd
Amatsuka4 <amatsuka@unangelic-angel.com>
2025-06-30 04:28:26 +0900

    [update] Shopコンポーネントに店舗名とダミーの説明文を追加し、画像の角を丸めるスタイルを変更

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -9,14 +9,19 @@ export default function Shop({ ramens }: { ramens: Shop[] }) {
 		<>
 			<main className="py-10 w-full">
 				<div className="w-3/4 xl:w-1/2 mx-auto">
-					<p className="text-2xl text-center mb-8">ID: {shop?.name}</p>
-					<div className="flex justify-center">
+					<div className="flex justify-center gap-16">
 						<div className="w-1/2">
 							<img
-								className="w-full aspect-square object-cover rounded-lg"
+								className="w-full aspect-square object-cover rounded-md"
 								src={shop?.photos?.[0]?.url || "https://placehold.co/600x600"}
 								alt="店舗画像"
 							/>
+						</div>
+						<div className="w-1/2">
+							<p className="text-2xl text-center mb-8">店名: {shop?.name}</p>
+							<p>
+								{"APIに説明文が無いため、ダミーの説明文を入れています。".repeat(15)}
+							</p>
 						</div>
 					</div>
 				</div>
