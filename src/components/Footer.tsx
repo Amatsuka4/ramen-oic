23f92d0e2d5f5c402d325ffdf572166e275935de
zeruku <zrcblue@gmail.com>
2025-07-01 08:11:26 +0900

    [update] Footerコンポーネントのスタイルを調整し、リンクリストの幅を変更。リストのマージンを修正し、レスポンシブデザインに対応。

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-	return (
-		<footer className="p-10 border-t border-gray-200 w-full">
-			<div className="w-1/4 mx-auto">
-				<ul className="flex items-center justify-center gap-10 mb-8 text-center">
-					<li className="w-full text-sm text-gray-600 hover:text-gray-800">
-						<Link to="/terms">規約</Link>
-					</li>
-					<li className="w-full text-sm text-gray-600 hover:text-gray-800">
-						<Link to="/privacy">プライバシーポリシー</Link>
-					</li>
-					<li className="w-full text-sm text-gray-600 hover:text-gray-800">
-						<Link to="/about">会社概要</Link>
-					</li>
-				</ul>
-			</div>
-			<p className="text-xs text-gray-600 text-center">&copy; 2025 RAMEEEN</p>
-		</footer>
-	);
+    return (
+        <footer className="p-10 border-t border-gray-200 w-full">
+            <div className="w-1/2 mx-auto">
+                <ul className="flex items-center justify-center gap-10 mb-16 text-center xl:flex-row flex-col">
+                    <li className="w-full text-sm text-gray-600 hover:text-gray-800">
+                        <Link to="/terms">規約</Link>
+                    </li>
+                    <li className="w-full text-sm text-gray-600 hover:text-gray-800">
+                        <Link to="/privacy">プライバシーポリシー</Link>
+                    </li>
+                    <li className="w-full text-sm text-gray-600 hover:text-gray-800">
+                        <Link to="/about">会社概要</Link>
+                    </li>
+                </ul>
+            </div>
+            <p className="text-xs text-gray-600 text-center">
+                &copy; 2025 RAMEEEN
+            </p>
+        </footer>
+    );
 }
