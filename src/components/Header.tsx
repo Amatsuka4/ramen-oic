41bfe65ecf244f6f6227e9ba5824f208b41f9d60
zeruku <zrcblue@gmail.com>
2025-07-01 15:21:33 +0900

    [update] Headerコンポーネントで検索後に入力をクリアする機能を追加。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,6 +30,7 @@ export default function Header() {
                                 ) {
                                     e.preventDefault();
                                     navigate(`/search?q=${search}`);
+                                    setSearch("");
                                 }
                             }}
                         />
